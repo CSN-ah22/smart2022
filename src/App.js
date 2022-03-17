2fb327c7d5e411cbe4fc29638f6c166f53492b94
csa-22 <csa2676@naver.com>
2022-03-17 16:46:59 +0900

    Grid 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import faker from '@faker-js/faker';  //영문 버전의 fakerjs
 import faker_ko from '@faker-js/faker/locale/ko';  //한글 버전의 fakerjs
+import UserCard from './components/UserCard';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 import './App.css';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-
-function App() {
   const userDatas = [];
 
   while(userDatas.length < 5){
@@ -22,48 +16,20 @@ function App() {
     })
   }
 
+function App() {
   const userCards = userDatas.map((userData, idx) => {
-    return <Card sx={{ maxWidth: 345 }}>
-    <CardMedia
-      component="img"
-      height="140"
-      image={ userData.avatar }
-      alt="green iguana"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-       <div key={idx}>
-       <h4>{ userData.jobTitle }</h4>
-       <img src={ userData.avatar } alt="사용자 프로필용 아바타"></img>
-       <h5>{ userData.name }</h5>
-       {userData.email} <br />
-      {userData.phoneNo}
-     </div>
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Share</Button>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
-    // <div key={idx}>
-    //   <h4>{ userData.jobTitle }</h4>
-    //   <img src={ userData.avatar } alt="사용자 프로필용 아바타"></img>
-    //   <h5>{ userData.name }</h5>
-    //   {userData.email} <br />
-    //   {userData.phoneNo}
-    // </div>
+    return <Grid item xs={2} sm={4} md={4} key={idx}>
+       <UserCard userData={userData} idx={idx} />
+       </Grid>
   })
 
   console.log(userDatas)
   return (
-    <div className="App">
+    <Container maxWidth="lg" sx={{p:1}}>
+    <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
         {userCards}
-    </div>
+    </Grid>
+    </Container>
   );
 }
 
