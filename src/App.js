42bd0fb6157240880f47a8873e86a01e9f2061f0
Sunah <csa4550@gmail.com>
2022-03-24 16:58:18 +0900

    0324_코드분할

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
-import faker from '@faker-js/faker';  //영문 버전의 fakerjs
-import faker_ko from '@faker-js/faker/locale/ko';  //한글 버전의 fakerjs
-import UserCard from './components/UserCard';
-import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import Box from '@mui/material/Box';
 import './App.css';
-  const userDatas = [];
+import React, { useState } from 'react';
+import UserCardList from './components/UserCardList';
+import { makeUserDatas } from './Utils';
 
-  while(userDatas.length < 5){
-    userDatas.push({
-      avatar: faker.image.avatar(),
-      name: `${faker_ko.name.lastName()}${faker_ko.name.firstName()}`,
-      email: faker.internet.email(),
-      jobTitle: faker.name.jobTitle(),
-      phoneNo: faker.phone.phoneNumber()
-    })
-  }
+const userDatas = makeUserDatas(5);
+
+  const UserDatas = [];
+
+
 
 function App() {
-  const userCards = userDatas.map((userData, idx) => {
-    return <Grid item xs={2} sm={4} md={4} key={idx}>
-       <UserCard userData={userData} idx={idx} />
-       </Grid>
-  })
 
-  console.log(userDatas)
+  const [useDarkMode, setUseDarkMode] = useState(true);
+
+  
+  const handleChange = (event) => {
+    console.log(event)
+    setUseDarkMode(event.target.checked);
+  };
+
+  // useEffect(() => {
+  //   console.log("component did mount")
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log(`theme 변경됨 -> ${useDarkMode}`)
+  // }, [useDarkMode]);
+  
+
   return (
+    <ThemeProvider theme={createTheme({
+      palette: {
+        mode: useDarkMode? 'dark' : 'light',
+      },
+    })
+  }>
+    <Box sx={{height: '100%',bgcolor: 'background.default',color: 'text.primary',p: 1,}}>
+
+    <Switch
+      defaultChecked color="warning" 
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+      
     <Container maxWidth="lg" sx={{p:1}}>
-    <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
-        {userCards}
-    </Grid>
+
+    <UserCardList userDatas={userDatas}/>
+
     </Container>
+    </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/UserCardList.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
+import { Pagination } from '@mui/material';
+import UserCard from './UserCard';
+import { paginate } from '../Utils'
+
+function UserCardList(props){
+    const pageContentsCount = 6;
+    const [pageNo, setPageNo] = useState(1);
+    const [currntUserData, setcurrentUserData] = useState(props.userDatas, pageContentsCount, pageNo);
+
+    const handleChangePageNo = (event, value) => {
+        setPageNo(value);
+        setcurrentUserData(paginate(props.userDatas, pageContentsCount, value));
+      }
+
+    const userCards = currntUserData.map((userData, idx) => {
+        return <Grid item xs={2} sm={4} md={4} key={idx}>
+           <UserCard userData={userData} idx={idx} />
+           </Grid>
+      })
+
+    return  [
+        <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12}}>
+        {userCards}
+    </Grid>,
+    
+    <Pagination
+    color="secondary"
+    count={Math.ceil(props.userDatas.length / pageContentsCount)}
+    page={pageNo}
+    onChange={handleChangePageNo}
+    />
+
+    ]
+}
+
+export default UserCardList;
